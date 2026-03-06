00c2a7b15679b53081a3e2bad7277a8faeea7830
Francesco Grazioso <francescograzioso1995@gmail.com>
2026-03-06 15:18:46 +0100

    chore(build): increment version code and name for new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.meld.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.0"
+        versionCode = 7
+        versionName = "0.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
