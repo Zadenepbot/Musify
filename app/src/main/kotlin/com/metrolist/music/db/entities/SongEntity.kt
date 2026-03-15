alltechdev/Metrolist-WEB_REMIX

feat(downloads): add format picker and custom download path support
- Add format picker dialog when downloading songs (shows bitrate/codec)
- Add custom download path support via SAF (Storage Access Framework)
- Export downloads to user-selected folder with Artist/Title structure
- Integrate Bento4 native library for M4A metadata embedding (cover art, title, artist, album, year)
- Add "swap download" option to re-download with different quality
- Add database schema migration for downloadUri tracking
- Only embed metadata for M4A files >= 128kbps (low bitrate compatibility)
- Show metadata badge on format options that support embedding

app/src/main/kotlin/com/metrolist/music/db/entities/SongEntity.kt
@@ -55,7 +55,9 @@ data class SongEntity(
     @ColumnInfo(name = "isUploaded", defaultValue = false.toString())
     val isUploaded: Boolean = false,
     @ColumnInfo(name = "isVideo", defaultValue = false.toString())
-    val isVideo: Boolean = false
+    val isVideo: Boolean = false,
+    @ColumnInfo(name = "downloadUri", defaultValue = "NULL")
+    val downloadUri: String? = null
 ) {
     fun localToggleLike() = copy(
         liked = !liked,

Zadenepbot/Musify

app/src/main/kotlin/com/metrolist/music/db/entities/SongEntity.kt
@@ -63,7 +63,9 @@ data class SongEntity(
     @ColumnInfo(name = "uploadEntityId", defaultValue = "NULL")
     val uploadEntityId: String? = null,
     @ColumnInfo(name = "isCached", defaultValue = "0")
-    val isCached: Boolean = false
+    val isCached: Boolean = false,
+    @ColumnInfo(name = "downloadUri", defaultValue = "NULL")
+    val downloadUri: String? = null
 ) {
     fun localToggleLike() = copy(
         liked = !liked,