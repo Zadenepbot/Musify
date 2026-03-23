b36efd09e707c8bb24df773dc108b4cb0e84dc08
grill-glitch <grill-glitch@users.noreply.github.com>
2026-03-24 06:06:35 +0800

    feat(netease): add coroutines dependency

--- netease/build.gradle.kts
+++ netease/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
+    implementation(libs.coroutines.guava)
     testImplementation(libs.junit)
 
     coreLibraryDesugaring(libs.desugaring)
