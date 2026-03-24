3cde0f3f359b772a85d3b77db52b008935e1c25b
Lurux <lurux-dev@protonmail.com>
2026-03-24 19:05:43 +0100

    Nuke legacy Screens

--- app/src/main/kotlin/com/metrolist/music/ui/utils/NavControllerUtils.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/utils/NavControllerUtils.kt
@@ -6,14 +6,9 @@
 package com.metrolist.music.ui.utils
 
 import androidx.navigation.NavController
-import com.metrolist.music.ui.screens.Screens
 
 fun NavController.backToMain() {
-    val mainRoutes = Screens.MainScreens.map { it.route }
-
-    while (previousBackStackEntry != null &&
-        currentBackStackEntry?.destination?.route !in mainRoutes
-    ) {
+    while (previousBackStackEntry != null) {
         popBackStack()
     }
 }
